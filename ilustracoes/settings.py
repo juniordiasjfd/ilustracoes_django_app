4a55ae35b436cd148d3b9df1d228e9481b24c329
Junior Dias <juniordiasjfd@gmail.com>
2025-11-13 19:49:40 -0300

    alteração para PostgreSQL

--- ilustracoes/settings.py
+++ ilustracoes/settings.py
@@ -85,10 +85,20 @@ WSGI_APPLICATION = 'ilustracoes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'geri_db',        # O nome que você criou
+        'USER': 'django_app_user',           # O usuário que você criou
+        'PASSWORD': 'Scriba123',     # A senha desse usuário
+        'HOST': 'localhost',                 # O servidor é seu próprio computador
+        'PORT': '5432',                      # A porta padrão do PostgreSQL
     }
 }
 
