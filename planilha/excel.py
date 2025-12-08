f83d5ca518ae92281135b8bef504e17575564f86
Junior Dias <juniordiasjfd@gmail.com>
2025-12-08 20:18:23 -0300

    início da construção do excel modelo de importação

---
+++ planilha/excel.py
@@ -0,0 +1,31 @@
+# ====================================================================
+# 1. CONFIGURAÇÃO MANDATÓRIA DO AMBIENTE DJANGO
+# ====================================================================
+import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ilustracoes.settings')
+django.setup()
+
+# ====================================================================
+# 2. IMPORTAÇÕES DE MODELOS
+# ====================================================================
+import pandas
+from planilha.models import Ilustracao, Ilustrador, Credito, Projeto, Componente
+
+def obter_verbose_names(model_class):
+    """Retorna uma lista de verbose_names para os campos do modelo."""
+    verbose_names = []
+    # O atributo '_meta' contém metadados do modelo
+    for field in model_class._meta.fields:
+        # Cada campo tem um atributo 'verbose_name'
+        verbose_names.append(field.verbose_name)
+    return verbose_names
+
+
+obter_verbose_names(Ilustracao)
+
+
+
+
+
+
